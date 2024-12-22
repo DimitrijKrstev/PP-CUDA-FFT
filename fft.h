de7540b412ad455e134edb419ff4f764d57dccf1
DimitrijKrstev <antishadowss@gmail.com>
2024-12-22 12:18:52 +0100

    add file read function to header

--- fft.h
+++ fft.h
@@ -18,6 +18,33 @@ void print_complex_array(const char* name, cuDoubleComplex* arr, int N) {
     printf("\n");
 }
 
+int read_file_input(const char* input_file, uint32_t N, cuDoubleComplex* input){
+    if (N & (N - 1)) {
+        fprintf(stderr, "N=%u must be a power of 2.\n", N);
+        return EXIT_FAILURE;
+    }
+
+    FILE* fp = fopen(input_file, "r");
+    if (!fp) {
+        perror("Error opening input file");
+        return EXIT_FAILURE;
+    }
+
+    for (uint32_t i = 0; i < N; i++) {
+        double real, imag;
+        if (fscanf(fp, "%lf %lf", &real, &imag) != 2) {
+            fprintf(stderr, "Error reading input data at index %u\n", i);
+            fclose(fp);
+            free(input);
+            return EXIT_FAILURE;
+        }
+        input[i] = make_cuDoubleComplex(real, imag);
+    }
+    fclose(fp);
+
+    return EXIT_SUCCESS;
+}
+
 cuDoubleComplex testInput[] = {
   make_cuDoubleComplex(3.6f, 2.6f),
   make_cuDoubleComplex(2.9f, 6.3f),
